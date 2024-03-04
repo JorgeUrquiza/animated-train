8f9abb6597556abbf460cedc68a70d823dd5bc2e
Jorge <jor.urq92@gmail.com>
2024-03-04 09:57:01 -0300

    subiendo imagen a subcarpetas

--- src/presentation/file-upload/controller.ts
+++ src/presentation/file-upload/controller.ts
@@ -28,7 +28,12 @@ export class FileUploadController {
     //? Cargar un archivo
     uploadFile = (req: Request, res: Response) => {
 
-        const files = req.files; // Tomar los archivos de la peticion
+        const type = req.params.type; // Tomar el tipo de archivo que se subira, type por que lo definimos en routes
+        const validTypes = ['users', 'products', 'categories']; // Tipos validos
+        if ( !validTypes.includes(type) ) {
+            return res.status(400)
+                .json({ error: `invalid type: ${ type}, valid ones ${ validTypes }` });
+        }
         
         if ( !req.files || Object.keys(req.files).length === 0 ) { // Si no se subio ningun archivo 
             return res.status(400).json({ error: 'No files were uploaded.' });
@@ -36,7 +41,7 @@ export class FileUploadController {
 
         const file = req.files.file as UploadedFile; // Tomar el archivo de la peticion 
 
-        this.fileUploadService.uploadSingle( file )
+        this.fileUploadService.uploadSingle( file, `uploads/${ type }` )  // Subir el archivo al servidor resolviendo la ruta donde se guardara
             .then( uploaded => res.json(uploaded) )
             .catch( error => this.handleError(error, res) ) // Manejar errores
 
